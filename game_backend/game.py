7077541a8b767f3199a9528480f9842aa1787c94
gaetan rigaut <gaetanrigaut.s@gmail.com>
2021-05-10 22:27:25 +0200

    résolution taille fenetre : fenetre par défaut trop grande

--- game_backend/game.py
+++ game_backend/game.py
@@ -3,13 +3,14 @@ from .player import Player
 
 
 class Game:
-    def __init__(self, width=96, height=32):
+    def __init__(self, width=64, height=32):
         self._generator = Generator(width=width, height=height)
         self._generator.gen_level()
         self._generator.gen_tiles_level()
         self._map = self._generator.tiles_level
         
-        self._player = Player()
+        
+        self._player = Player(symbol="P1")
         self._player.initPos( self._map )
 
     def getMap(self):
